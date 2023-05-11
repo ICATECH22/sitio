acf18892dba0fdba733c8cfeb9c5330b6f83541e
danielmendez88 <daniboymendez@gmail.com>
2023-05-11 15:20:20 -0600

    modificaciones de pronunciamiento

--- resources/views/theme/main/integridad/pronunciamiento.blade.php
+++ resources/views/theme/main/integridad/pronunciamiento.blade.php
@@ -8,7 +8,7 @@
             <img src="{{ asset('assets/img/banner/banner_pronunciamiento.jpeg') }}" alt=""  width="100%">
         </div>
     </div>
-
+    <br><br>
 {{-- agregar enlace de video vía FORM --}}
     <div class="vid-container">
         <iframe loading="lazy" id="vid_frame" width="100%" height="315" src="https://www.youtube.com/embed/eoDOcjNC9oE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
