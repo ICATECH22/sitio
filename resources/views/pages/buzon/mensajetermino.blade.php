0da989f53f39991a0d9a04da0ced96cda6d7709a
danielmendez88 <daniboymendez@gmail.com>
2023-09-20 15:53:26 -0600

    modificaciones de mensaje terminado

--- resources/views/pages/buzon/mensajetermino.blade.php
+++ resources/views/pages/buzon/mensajetermino.blade.php
@@ -5,21 +5,7 @@
 @section('csscontent')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <style>
-        .banner {
-            width: 100%;
-            height: auto;
-            padding-bottom: 0.8em;
-            padding-top: 1.5em;
-        }
-        .stilo_texto{
-			font-weight: bold;
-			font-family: Arial, Helvetica, sans-serif;
-			font-size: 1.5em;
-			color: 	#000000;
-			text-align: justify;
-		}
-    </style>
+    <link rel="stylesheet" href="{{ asset('assets/css/buzon.css') }}">
 @endsection
 
 @section('contenido')
