ea826be382f14acd038ab6ed9057aa78c201d4cf
danielmendez88 <daniboymendez@gmail.com>
2023-09-21 09:53:32 -0600

    modificaciones mensaje termino

--- resources/views/pages/buzon/mensajetermino.blade.php
+++ resources/views/pages/buzon/mensajetermino.blade.php
@@ -2,11 +2,11 @@
 
 @section('title', 'Buzón Digital Ciudadano | Icatech')
 
-{{-- @section('csscontent')
+@section('csscontent')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/buzon.css') }}">
-@endsection --}}
+@endsection
 
 @section('contenido')
     <div class="container">
@@ -21,7 +21,7 @@
                         Su solicitud ha sido enviada satisfactoriamente. Gracias por tomarte el tiempo para dejarnos tu comentario en nuestro buzón digital.
                     </p>
 
-                    <a type="button" href="{{ route('home') }}" class="btn btn-lg" style="background-color: #621132; color: #FFF;">
+                    <a type="button" href="{{ url('/') }}" class="btn btn-lg" style="background-color: #621132; color: #FFF;">
                         <i class="fas fa-home"></i>
                         Inicio
                     </a>
@@ -31,9 +31,9 @@
         </div>
     </div>
 @endsection
-{{-- @section('contentScriptJs')
+@section('contentScriptJs')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script src="{{ asset('assets/js_/init-scripts/jquery-validation/dist/jquery.validate.js') }}"></script>
     <script src="{{ asset('assets/js_/init-scripts/jquery-validation/dist/additional-methods.min.js') }}"></script>
     <script src="{{ asset('assets/js/dist/jquery.inputmask.js') }}"></script>
-@endsection --}}
+@endsection
