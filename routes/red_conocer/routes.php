e75227271ea564d27f72204a70849619bf42e140
danielmendez88 <daniboymendez@gmail.com>
2024-01-18 20:01:13 -0600

    modificar rutas red conocer

--- routes/red_conocer/routes.php
+++ routes/red_conocer/routes.php
@@ -4,9 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ece\EceController;
 
 Route::get('/ece-icatech/inicio', [EceController::class, 'index'])->name('ece_icatech.index');
-Route::get('/ece-icatech/acerca-de-nosotros', [EceController::class, 'index'])->name('ece_icatech.acercade');
 // certificaciones
-Route::get('/ece-icatech/certificaciones', [EceController::class, 'index'])->name('ece_icatech.certificaciones');
+// Route::get('/ece-icatech/certificaciones', [EceController::class, 'index'])->name('ece_icatech.certificaciones');
 // contactanos
 Route::get('/ece-icatech/contactanos', [EceController::class, 'contact'])->name('ece_icatech.contactanos');
 // about us
