4ae998c0b2f68001d2b6c781e868ae7719dc3557
danielmendez88 <daniboymendez@gmail.com>
2024-09-12 13:18:14 -0600

    modificacion quitar archivo del anio 2022

--- app/Http/Controllers/principal/IntegridadController.php
+++ app/Http/Controllers/principal/IntegridadController.php
@@ -121,7 +121,6 @@ class IntegridadController extends Controller
     protected function getquejasodenuncias()
     {
         $array_atencion_quejas_denuncias = array(
-            'Protocolo de Atención de Denuncias 2022' => 'codigos_conducta/PROTOCOLO_DE_ATENCION_DE_DENUNCIAS_2022.pdf',
             'Protocolo para la Prevención, Atención y Sanción del Hostigamiento Sexual y Acoso Sexual 2024' => 'codigos_conducta/protocolo_para_la_prevencion_atencion_y_sancion_del_hostigamiento_sexual_y_acoso_sexual_2024.pdf',
         );
         $bprincipal = $this->getBanner('banner_principal');
